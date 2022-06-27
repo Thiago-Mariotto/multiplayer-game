f23e8cad05dc2512a23f3ad784d72ca0cb1a17f2
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-06-27 15:36:00 -0300

    feat: add initial server game

---
+++ server/index.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import * as http from 'http';
+import { Server, Socket } from 'socket.io';
+import { createPlayer, updatePlayer } from './config/player';
+import { Player } from './entities/player';
+import { ClientToServerEvents, InterServerEvents, ServerToClientEvents, SocketData } from './entities/socketio';
+
+const port = 3000;
+const app = express();
+const server = http.createServer(app);
+const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(server);
+let players: Array<Player> = [];
+
+app.use(express.static('../public'));
+
+io.on('connection', (socket: Socket<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>) => {
+	console.log(`${socket.id} now is connected`);
+	const data = {
+		id: socket.id,
+		nickname: socket.id
+	};
+
+	const newPlayer = createPlayer(data);
+	players.push(newPlayer);
+
+	io.emit('connectPlayer', players);
+
+	socket.on('movement', (movement) => {
+		players = updatePlayer(players, socket.id, movement);
+		io.emit('playerMovement', players);
+	});
+
+	socket.on('disconnect', () => {
+		console.log(`${socket.id} now is disconnected`);
+		players = players.filter((p) => p.id !== socket.id);
+		io.emit('connectPlayer', players);
+	});
+});
+
+server.listen(port, () => console.log(`App running on port  ${port}!`));
